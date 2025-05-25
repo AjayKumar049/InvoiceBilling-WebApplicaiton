6904b48253167bbd1d2829ece161706c90d13693
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-05-25 11:38:31 +0530

    Update BackendApplication.java

--- Backend/Backend/src/main/java/com/example/Backend/BackendApplication.java
+++ Backend/Backend/src/main/java/com/example/Backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.example.Backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class BackendApplication {
 
 	public static void main(String[] args) {
