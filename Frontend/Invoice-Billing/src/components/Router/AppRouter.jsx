1532e0446009af2a5bd53dcdddf03c967e857469
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-05-26 11:33:37 +0530

    Add files via upload

---
+++ Frontend/Invoice-Billing/src/components/Router/AppRouter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+import Applayout from "../components/layouts/Applayout"; 
+import Signup from "../components/pages/Signup";
+import Login from "../components/pages/Login"; 
+import PageNotFound from "../components/pages/PageNotFound";
+//import Notfound from "../components/Notfound";
+
+const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Applayout />, 
+    children: [
+      {
+        path: "/",
+        element: <Signup />,
+      },
+      {
+        path: "/login", 
+        element: <Login />,
+      },
+      {
+        path: "/*", 
+        element: <PageNotFound />,
+      }
+     
+    ]
+  }
+]);
+
+export default AppRouter;
