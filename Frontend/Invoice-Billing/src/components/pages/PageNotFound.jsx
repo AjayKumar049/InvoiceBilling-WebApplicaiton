7f5ddfb1b925ed6788275ab1bb84791e99341131
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-05-26 12:37:44 +0530

    Update PageNotFound.jsx

--- Frontend/Invoice-Billing/src/components/pages/PageNotFound.jsx
+++ Frontend/Invoice-Billing/src/components/pages/PageNotFound.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <>
-    <h1>404-PageNotfound-Invalid URL</h1></>
-  )
-}
+    <div className="p-6 w-fit mx-auto mt-20 text-center">
+      <h1 className="text-2xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="mb-4">The requested page does not exist</p>
+      <Link className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" to="/">Go back to home</Link>
+    </div>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
